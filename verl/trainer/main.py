d1f36114fddca868e0b657f89527550f95f8a4a5
muxixi727 <63194707+muxixi727@users.noreply.github.com>
2025-03-01 10:36:28 +0800

    Update main.py

--- verl/trainer/main.py
+++ verl/trainer/main.py
@@ -74,7 +74,7 @@ def main_task(config: PPOConfig):
         tokenizer=tokenizer, num_examine=1, compute_score=config.worker.reward.compute_score, max_length = config.data.max_response_length
     )
     val_reward_fn = CustomRewardManager(
-        tokenizer=tokenizer, num_examine=1, compute_score=config.worker.reward.compute_score
+        tokenizer=tokenizer, num_examine=1, compute_score=config.worker.reward.compute_score, max_length = config.data.max_response_length
     )
 
     resource_pool_manager = ResourcePoolManager(resource_pool_spec=resource_pool_spec, mapping=mapping)
