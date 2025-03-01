f83b1bc514b4173cfa570b2e1455df6cfa356e33
muxixi727 <63194707+muxixi727@users.noreply.github.com>
2025-03-01 11:00:16 +0800

    Update run_qwen2_5_vl_3b_geo.sh

--- examples/run_qwen2_5_vl_3b_geo.sh
+++ examples/run_qwen2_5_vl_3b_geo.sh
@@ -3,12 +3,12 @@ set -x
 export VLLM_ATTENTION_BACKEND=XFORMERS
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
-MODEL_PATH=/lpai/models/qwen2-5-vl-3b-instruct/25-02-18-1  # replace it with your local file path
+MODEL_PATH=/lpai/inputs/models/qwen2-5-vl-3b-instruct-25-02-18-1  # replace it with your local file path
 
 python3 -m verl.trainer.main \
     config=grpo_example.yaml \
-    data.train_files=/lpai/data_new/train \
-    data.val_files=/lpai/data_new/test \
+    data.train_files=/lpai/output/train \
+    data.val_files=/lpai/output/test \
     worker.actor.model.model_path=${MODEL_PATH} \
     worker.actor.kl_loss_coef=0.05 \
     worker.rollout.tensor_parallel_size=1 \
